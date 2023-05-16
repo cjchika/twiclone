f5fbe81db73c0301df6767c13a2684e6adf7e3c5
Chijioke Chika <8pluscreative@gmail.com>
2023-05-16 10:04:07 +0100

    Auth

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,10 +1,38 @@
-import 'package:appwrite/appwrite.dart' as model;
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:twiclone/core/core.dart';
 
+final authAPIProvider = Provider((ref) {});
+
 abstract class IAuthAPI {
-  FutureEither<model.Account> signUp({
+  FutureEither<model.User> signUp({
     required String email,
     required String password,
-})
+  });
+}
 
+class AuthAPI implements IAuthAPI {
+  AuthAPI({required Account account}) : _account = account;
+  final Account _account;
+
+  @override
+  FutureEither<model.User> signUp(
+      {required String email, required String password}) async {
+    try {
+      final account = await _account.create(
+        userId: ID.unique(),
+        email: email,
+        password: password,
+      );
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message??'Some unexpected error occurred', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+    }
+  }
 }
