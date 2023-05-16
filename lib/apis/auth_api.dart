cce6e4f4ebb3ea875dd893dd15ca1d224181a4c9
Chijioke Chika <8pluscreative@gmail.com>
2023-05-16 05:39:46 +0100

    Auth

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,8 +1,8 @@
-import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/appwrite.dart' as model;
 import 'package:twiclone/core/core.dart';
 
 abstract class IAuthAPI {
-  FutureEither<Account> signUp({
+  FutureEither<model.Account> signUp({
     required String email,
     required String password,
 })
