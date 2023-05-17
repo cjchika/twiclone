236667856ed14d2c9ceced1efc851c1cd79c22bc
Chijioke Chika <8pluscreative@gmail.com>
2023-05-17 12:20:54 +0100

    Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twiclone/common/common.dart';
 import 'package:twiclone/common/loading_page.dart';
 import 'package:twiclone/features/auth/controller/auth_controller.dart';
 
 // import 'package:twiclone/features/auth/view/login_view.dart';
 import 'package:twiclone/features/auth/view/signup_view.dart';
+import 'package:twiclone/features/home/view/home_view.dart';
 import 'package:twiclone/theme/theme.dart';
 
 void main() {
@@ -26,8 +28,13 @@ class MyApp extends ConsumerWidget {
       title: 'Flutter Demo',
       theme: AppTheme.theme,
       home: ref.watch(currentUserAccountProvider).when(
-          data: (user) {},
-          error: (error, st) {},
+          data: (user) {
+            if (user != null) {
+              return const HomeView();
+            }
+            return const SignUpView();
+          },
+          error: (error, st) => ErrorPage(error: error.toString()),
           loading: () => const LoadingPage()),
     );
   }
